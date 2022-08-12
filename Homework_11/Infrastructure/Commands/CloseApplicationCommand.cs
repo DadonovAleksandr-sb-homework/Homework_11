2299428605a86081181fdbcf6133d51b69566602
DadonovAD <DadonovAD@tvv.transneft.ru>
2022-08-12 10:01:21 +0300

    Заготовка под MVVM

---
+++ Homework_11/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using Homework_11.Infrastructure.Commands.Base;
+
+namespace Homework_11.Infrastructure.Commands;
+
+internal class CloseApplicationCommand : Command
+{
+    public override bool CanExecute(object parameter) => true;
+
+
+    public override void Execute(object parameter) => Application.Current.Shutdown();
+        
+}
